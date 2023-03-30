532fa2866e2f55e1e4410be8eca3cc74735ed754
GitHub Action <action@github.com>
2023-03-30 02:25:01 +0000

    Template cleanup

--- src/main/kotlin/com/github/buhe/hellojet/listeners/MyFrameStateListener.kt
+++ src/main/kotlin/com/github/buhe/hellojet/listeners/MyFrameStateListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.buhe.hellojet.listeners
 
 import com.intellij.ide.FrameStateListener
 import com.intellij.openapi.diagnostic.thisLogger
